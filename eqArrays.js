9a0b020463237617ed4d9290444fd71f3c537772
Dexter Chan <dexterchann@gmail.com>
2020-03-03 17:26:42 +0000

    Created eqArray.js file to compare arrays

---
+++ eqArrays.js
@@ -0,0 +1,26 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`"✅ Assertion Passed: ${actual} === ${expected}"`);
+  } else {
+    console.log(`"❌ Assertion Failed: ${actual} !== ${expected}"`);
+  }
+};
+
+const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  }
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+// Test cases
+// assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+// assertEqual(eqArrays([1, 2, 3], [1, 2, 3, 4]), false);
+// assertEqual(eqArrays([1, 2, 4], [1, 2, 3]), false);
+// assertEqual(eqArrays([1, 2, 4], [1, 2, "3"]), false);
+// assertEqual(eqArrays([1], [1]), true);
