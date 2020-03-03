d55177f00f3af8244644921c0ca947d1fe9303ba
Dexter Chan <dexterchann@gmail.com>
2020-03-03 17:43:16 +0000

    Refactored assertArraysEqual function to call eqArrays function

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,24 +1,25 @@
-const assertArraysEqual = function(array1, array2) {
-  let isEqual = true;
+const eqArrays = function(array1, array2) {
   if (array1.length !== array2.length) {
-    isEqual = false;
+    return false;
   }
-
   for (let i = 0; i < array1.length; i++) {
     if (array1[i] !== array2[i]) {
-      isEqual = false;
+      return false;
     }
   }
+  return true;
+}
 
-  if (isEqual === true) {
+const assertArraysEqual = function(array1, array2) {
+  if (eqArrays(array1, array2)) {
     console.log(`"✅ Assertion Passed: ${array1} === ${array2}"`);
   } else {
     console.log(`"❌ Assertion Failed: ${array1} !== ${array2}"`);
   }
 }
 
-// // Test cases
-// assertArraysEqual([1, 2, 3], [1, 2, 3]);
-// assertArraysEqual([1, 2, 3], [1, 2, 4]);
-// assertArraysEqual([1, 2, 3], [1, 2, "3"]);
-// assertArraysEqual([1, 2, 3], [1, 2, 3, 4]);
+// Test cases
+assertArraysEqual([1, 2, 3], [1, 2, 3]);
+assertArraysEqual([1, 2, 3], [1, 2, 4]);
+assertArraysEqual([1, 2, 3], [1, 2, "3"]);
+assertArraysEqual([1, 2, 3], [1, 2, 3, 4]);
