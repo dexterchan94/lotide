05c4bc6cc2954328da263f759e7f6d0c22bd4544
Dexter Chan <dexterchann@gmail.com>
2020-03-09 16:33:01 +0000

    Refactored

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,14 +1,4 @@
-const eqArrays = function(array1, array2) {
-  if (array1.length !== array2.length) {
-    return false;
-  }
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
+const eqArrays = require("./eqArrays");
 
 const assertArraysEqual = function(array1, array2) {
   if (eqArrays(array1, array2)) {
@@ -18,8 +8,5 @@ const assertArraysEqual = function(array1, array2) {
   }
 };
 
-// Test cases
-assertArraysEqual([1, 2, 3], [1, 2, 3]);
-assertArraysEqual([1, 2, 3], [1, 2, 4]);
-assertArraysEqual([1, 2, 3], [1, 2, "3"]);
-assertArraysEqual([1, 2, 3], [1, 2, 3, 4]);
+module.exports = assertArraysEqual;
+
