bef52e28f4a21e50856958cede4ba209e8369700
Dexter Chan <dexterchann@gmail.com>
2020-03-09 16:38:42 +0000

    Refactored

--- middle.js
+++ middle.js
@@ -1,23 +1,3 @@
-const eqArrays = function(array1, array2) {
-  if (array1.length !== array2.length) {
-    return false;
-  }
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
-
-const assertArraysEqual = function(array1, array2) {
-  if (eqArrays(array1, array2)) {
-    console.log(`"✅ Assertion Passed: ${array1} === ${array2}"`);
-  } else {
-    console.log(`"❌ Assertion Failed: ${array1} !== ${array2}"`);
-  }
-};
-
 const middle = function(array) {
   if (array.length <= 2) {
     return [];
@@ -28,10 +8,4 @@ const middle = function(array) {
   }
 };
 
-// // Test cases
-// assertArraysEqual(middle([]), []);
-// assertArraysEqual(middle([1]), []);
-// assertArraysEqual(middle([1, 2]), []);
-// assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
-// assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
-// assertArraysEqual(middle([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]), [5, 6]);
+module.exports = middle;
