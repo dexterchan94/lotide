0132c955123dbcb546558e309259a733c3b21eaf
Dexter Chan <dexterchann@gmail.com>
2020-03-09 16:14:14 +0000

    Refactored assertEqual and head and added test files for each

--- assertEqual.js
+++ assertEqual.js
@@ -6,8 +6,5 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-// TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual("Potato", "Potato");
-assertEqual(1, 1);
-assertEqual(1, 2);
+module.exports = assertEqual;
+
